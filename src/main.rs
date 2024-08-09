82f5405337bb4a9f0933c7e81034938620a198c7
supersanta183 <klemmensen11@gmail.com>
2024-08-09 02:37:18 +0200

    new fee

--- src/main.rs
+++ src/main.rs
@@ -257,10 +257,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     let difficulty = solution.to_hash().difficulty();
 
                     let mut transaction_fee = 0;
-                    if difficulty < 22 {
-                        transaction_fee = 50000;
+                    if difficulty < 24 {
+                        transaction_fee = 20000;
                     } else {
-                        transaction_fee = 100000;
+                        transaction_fee = 50000;
                     }
 
                     let prio_fee_ix = ComputeBudgetInstruction::set_compute_unit_price(transaction_fee);
